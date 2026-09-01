98bf0284dd84ec8dc06efb1a98330a4e8d60896d
immortaldotjar <arjun.pra1611@gmail.com>
2026-09-01 07:12:30 +0530

    Ui setup

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import { Outlet } from 'react-router-dom'
+const MainLayout = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default MainLayout
